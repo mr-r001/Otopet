94131c28adec6d8aa90ea5fae379f212020de47a
valclassdevelop <developervalclass@gmail.com>
2021-12-03 14:24:53 +0700

    add file model.css

--- resources/views/admin/B11/show.blade.php
+++ resources/views/admin/B11/show.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>model B.1.1-KWK</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="{{ asset('backend/css/model.css') }}">
 </head>
 <body>
     <section class="hero">
